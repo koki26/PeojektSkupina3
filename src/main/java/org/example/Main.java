3a35c0a2295236ac9ca7fc7dcf291850df623810
VrbaMirek <vrba.m@ssmg.cz>
2025-11-12 11:42:55 +0100

    MV

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        // sef
+        // sef MV
         // +2 zakaznicy
         // ukládání ()
         // vztup ()
